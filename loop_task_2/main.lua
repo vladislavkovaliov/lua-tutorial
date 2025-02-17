bdd345e020a010a7aacc84a56f14567bcdaf463f
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2025-02-17 21:16:09 +0300

    chore: adds task about count even and odd digit in the number inputed

---
+++ loop_task_2/main.lua
@@ -0,0 +1,37 @@
+--[[
+
+Определить, сколько в числе четных цифр, 
+а сколько нечетных. Число вводится с клавиатуры
+
+]]
+
+local countEven = 0;
+local countOdd = 0;
+
+---@param number number
+---@return boolean
+local function isEven(number)
+    return number % 2 == 0;
+end
+
+local number = 0;
+
+repeat
+    io.write("Input number: ");
+
+    number = tonumber(io.read());
+until number ~= nil;
+
+while number > 0 do
+    local digit = number % 10;
+
+    if isEven(digit) then
+        countEven = countEven + 1;
+    else
+        countOdd = countOdd + 1;
+    end
+
+    number = math.floor(number / 10);
+end
+
+print(string.format("Even: %d, Odd: %d", countEven, countOdd));
